c091eceb475c0601a99b74696db849dd5c3160cf
EC2 Default User <dachengjiana76@gmail.com>
2022-12-15 06:51:34 +0000

    12/15

---
+++ セッション５/04_for break continue.py
@@ -0,0 +1,21 @@
+#some_list = [1,2,3,4,5]
+
+#i = 0
+#while i < len(some_list):
+    #print(some_list[i])
+    #i += 1
+
+#for を使った場合
+#for i in some_list:
+ # print(i)
+
+#文字列の場合
+
+#for s in 'abcde':
+ #   print(s)
+
+for word in['My','name','is','Mike']:
+  if word == 'name':
+      continue
+  print(word)
+ 
---
+++ セッション５/08_zip.py
@@ -0,0 +1,6 @@
+days = ['Mon','Tue','Wed']
+fruits = ['apple','banana','orange']
+drinks = ['coffee','tea','beer']
+
+for day,fruits,drink in zip(days,fruits,drinks):
+  print(day,fruits,drink)
